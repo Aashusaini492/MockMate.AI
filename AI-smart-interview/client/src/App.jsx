8cb0417bccbae001e38dfb15b8dbd8bcb1fbc57a
aashu <aashusaini.cse23@gmail.com>
2026-07-10 20:05:29 +0530

    Fixed authentication, backend connection and updated project

--- AI-smart-interview/client/src/App.jsx
+++ AI-smart-interview/client/src/App.jsx
@@ -10,6 +10,7 @@ import InterviewPage from './pages/InterviewPage'
 import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
+export const ServerUrl = "http://localhost:8000";
 
 
 
